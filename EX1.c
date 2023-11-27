1d83fb6d11eb856c5dad2bfe64252386111ca312
aurélien <150128939+alphaDIX@users.noreply.github.com>
2023-11-27 09:31:59 +0100

    Add files via upload
    Exercice 1 -> welcome message in the prompt

---
+++ EX1.c
@@ -0,0 +1,8 @@
+/*
+ * EX1.c
+ *
+ *  Created on: 27 nov. 2023
+ *      Author: ensea
+ */
+
+
---
+++ EX1.h
@@ -0,0 +1,13 @@
+/*
+ * EX1.h
+ *
+ *  Created on: 27 nov. 2023
+ *      Author: ensea
+ */
+
+#ifndef EX1_H_
+#define EX1_H_
+
+
+
+#endif /* EX1_H_ */
