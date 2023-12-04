ed2424d7f2066c2d353e8e8edffab6d2f48b2458
aurélien <150128939+alphaDIX@users.noreply.github.com>
2023-12-04 20:21:16 +0100

    Add files via upload

--- EX4.c
+++ EX4.c
@@ -14,18 +14,17 @@
 #include <sys/wait.h>
 #include <string.h>
 #include <stdbool.h>
+#include <time.h>
 
 #define buffsize 128
 const char * welcome = "$ ./enseash\nBienvenue dans le shell ENSEA.\nPour quitter, tapez 'exit'.\nenseash % ";
-const char * ensea = "enseash % ";
+const char * ensea = "\nenseash % ";
 const char * goodbye = "Bye bye...\n$";
 
 int main(int argc, char * argv[]){
 
 	char buffer[buffsize];
 	ssize_t ret;
-	int time_1;
-	int time_2;
 
 	if(write(STDOUT_FILENO, welcome, strlen(welcome)) == -1){perror("write");exit(EXIT_FAILURE);} //writing the welcome message
 
@@ -48,23 +47,24 @@ int main(int argc, char * argv[]){
 				} else if(waitpid(pid,&status,0) == -1){ //the father process is waiting for the child to finish
 					perror("wait");
 					exit(EXIT_FAILURE);}
+
 				buffer[0] = '\0'; //emptying the buffer
-
-				if ((WIFEXITED(status) != 0)&&(WEXITSTATUS(status) != 0)){ //checking if child ended correctly and that we have a return value
+				if ((WIFEXITED(status))){                       //checking if child ended correctly
 					int exit_status = WEXITSTATUS(status);
-					strcat(buffer, "enseash [exit:");
-					strcat(buffer, (char*) &exit_status);
+					strcat(buffer, "\nenseash [exit:");
+					if(write(STDOUT_FILENO, buffer, strlen(buffer)) == -1){perror("write");exit(EXIT_FAILURE);}
+					snprintf(buffer,sizeof(exit_status),"%d",exit_status);
 					strcat(buffer, "] % ");
-					if(write(STDOUT_FILENO, buffer, buffsize) == -1){perror("write");exit(EXIT_FAILURE);}
-				} else if((WIFSIGNALED(status) != 0)&&(WTERMSIG(status) != 0)){ //checking the signal that killed the child (if there is one)
+					if(write(STDOUT_FILENO, buffer, strlen(buffer)) == -1){perror("write");exit(EXIT_FAILURE);}
+				} else if((WIFSIGNALED(status))){              //checking the signal that killed the child (if there is one)
 					int signal_status = WTERMSIG(status);
-					strcat(buffer, "enseash [sign:");
-					strcat(buffer, (char*) &signal_status);
+					strcat(buffer, "\nenseash [sign:");
+					if(write(STDOUT_FILENO, buffer, strlen(buffer)) == -1){perror("write");exit(EXIT_FAILURE);}
+					snprintf(buffer,sizeof(signal_status),"%d",signal_status);
 					strcat(buffer, "] % ");
-					if(write(STDOUT_FILENO, buffer, buffsize) == -1){perror("write");exit(EXIT_FAILURE);}
-				}
+					if(write(STDOUT_FILENO, buffer, strlen(buffer)) == -1){perror("write");exit(EXIT_FAILURE);}
+				} else if(write(STDOUT_FILENO, ensea, strlen(ensea)) == -1){perror("write");exit(EXIT_FAILURE);} //writing the new line prompt message
 
-				if(write(STDOUT_FILENO, ensea, strlen(ensea)) == -1){perror("write");exit(EXIT_FAILURE);} //writing the new line prompt message
 
 		}
 
@@ -72,5 +72,4 @@ int main(int argc, char * argv[]){
 
 	exit(EXIT_SUCCESS);
 }
-
 
